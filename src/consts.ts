f3e287bf8cd81f9854077e7e3703c047ad601568
Ricardo Faria <ricardofcosta0804@gmail.com>
2025-06-16 15:27:47 -0300

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Ricardo Faria';
+export const SITE_DESCRIPTION = 'Blog, Artigos e Estudos';
